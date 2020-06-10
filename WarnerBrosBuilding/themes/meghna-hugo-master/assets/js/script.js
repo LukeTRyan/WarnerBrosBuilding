78ee6a6f8114aa7ecd1dea7158c4c4a213080b08
Luke Ryan <luke_t_ryan@hotmail.com>
2020-06-10 16:43:37 +1000

    lag fix

--- WarnerBrosBuilding/themes/meghna-hugo-master/assets/js/script.js
+++ WarnerBrosBuilding/themes/meghna-hugo-master/assets/js/script.js
@@ -39,32 +39,9 @@ jQuery(function ($) {
 	/*	Portfolio Filtering Hook
 	/* =========================================================================  */
 
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
-
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
-	}
-
 	/* ========================================================================= */
 	/*	Testimonial Carousel
 	/* =========================================================================  */
-
-	$("#testimonials").slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
-	});
 
 	/* ========================================================================= */
 	/*	animation scroll js
